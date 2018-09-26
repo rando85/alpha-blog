15ab006b7d4fede7e9a60371cc76a374942ca5ad
Daniel Cisneros <webmasterofpuppetz@gmail.com>
2018-09-26 18:32:19 -0500

    fix path function

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,7 +7,7 @@ class ArticlesController < ApplicationController
 		@article = Article.new(article_params)
 		if @article.save
 			flash[:notice] = "Article was successfully created"
-			redirect_to articles_path(@article)
+			redirect_to article_path(@article)
 		else
 			render 'new'
 		end
